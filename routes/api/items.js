885d56c0eb24224eac280321903c1bd9cf4ba6bd
chuksys <chuksagbakuru@gmail.com>
2021-04-25 10:39:21 +0100

    Added Routing functionality to routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const Item = require('../../models/Item')
+
 router.get('/', (req, res) => {
-    res.status(200).send('API Working')
+    return Item.find().sort({date: -1})
+    .then(items => {
+        res.status(200).send(items);
+    }).catch(err => res.status(500).send(err))
 })
 
 router.post('/', (req, res) => {
-    res.status(200).send('API Working')
+    const newItem = new Item({
+        name: req.body.name
+    })
+    return newItem.save()
+    .then(item => res.status(201).send(item))
+    .catch(err => res.status(500).send(err))
 })
 
-router.put('/', (req, res) => {
-    res.status(200).send('API Working')
+router.put('/:id', (req, res) => {
+    return Item.findByIdAndUpdate(req.params.id, {
+        name: req.body.name
+    }, {new: true}).then(item => res.status(201).send(item))
 })
 
-router.delete('/', (req, res) => {
-    res.status(200).send('API Working')
+router.delete('/:id', (req, res) => {
+    return Item.findByIdAndDelete(req.params.id)
+    .then(item => res.status(201).send(item))
 })
 
 module.exports = router
