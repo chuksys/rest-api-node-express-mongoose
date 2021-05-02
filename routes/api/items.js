ea7247c063980aab5fd351cea2c35efc44660973
Chuks Agbakuru <chuksagbakuru@gmail.com>
2021-05-02 08:59:19 +0100

    Added Routing functionality to routes (#4)
    * Added Routing functionality to routes
    
    * Edited README
    
    * Edited README
    
    * Edited README
    
    * Edited README
    
    * Edited REadmE
    
    * Edited README
    
    * Edited README
    
    * Edited README
    
    * Edited README
    
    * Edited README
    
    * Added Routing functionality to routes
    
    * Added validation & error handling to routes
    
    * fixed issue with Joi implementation
    
    * Used ES6 arrow functions in routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,20 +1,58 @@
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
 
+const Item = require('../../models/Item')
+
+const validateItem = item => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(item);  
+}
+
+
 router.get('/', (req, res) => {
-    res.status(200).send('API Working')
+    return Item.find().sort({date: -1})
+    .then(items => res.status(200).send(items))
+    .catch(err => res.status(500).send(err))
+})
+
+router.get('/:id', (req, res) => {
+    return Item.findById(req.params.id)
+                .then(item => res.status(200).send(item))
+                .catch(err => res.status(404).send(err))
 })
 
 router.post('/', (req, res) => {
-    res.status(200).send('API Working')
+    const { error } = validateItem(req.body)
+    if(error) return res.status(400).send(error.details);
+
+    const newItem = new Item({
+        name: req.body.name
+    })
+    return newItem.save()
+    .then(item => res.status(201).send(item))
+    .catch(err => res.status(500).send(err))
 })
 
-router.put('/', (req, res) => {
-    res.status(200).send('API Working')
+router.put('/:id', (req, res) => {
+    const { error } = validateItem(req.body)
+    if(error) res.status(400).send(error)
+
+    return Item.findByIdAndUpdate(req.params.id, {
+        name: req.body.name
+    }, {new: true})
+        .then(item => res.status(201).send(item)
+        .catch(err => res.status(404).send("Resource not found")))
 })
 
-router.delete('/', (req, res) => {
-    res.status(200).send('API Working')
+router.delete('/:id', (req, res) => {
+    return Item.findById(req.params.id)
+                .then(item => item.remove()
+                    .then(item => res.status(201).send(item))
+                    .catch(err => res.status(500).send(err)))
+                .catch(err => res.status(404).send('Resource not found'))
 })
 
 module.exports = router
